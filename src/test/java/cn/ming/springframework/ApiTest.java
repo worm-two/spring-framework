bbd039a8079faaa8366359550494b18b5a3b640b
策君公子开明 <gouxuming@gmail.com>
2023-07-22 12:44:03 +0800

    【手写简单Spring框架】初始化

--- src/test/java/cn/ming/springframework/ApiTest.java
+++ src/test/java/cn/ming/springframework/ApiTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class ApiTest {
 
     @Test
-    public void test_scan() {
+    public void scan() {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
         IUserService userService = applicationContext.getBean("userService", IUserService.class);
         System.out.println("测试结果：" + userService.queryUserInfo());
